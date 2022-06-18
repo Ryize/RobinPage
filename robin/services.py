7064389e3a49f9760aa359e680b99e8ac13abd8c
CMatvey <chekashovmatvey@gmail.com>
2022-06-19 00:32:25 +0300

    Added a limit on requests to the service (no more than 3 per day)

---
+++ robin/services.py
@@ -0,0 +1,68 @@
+import datetime
+
+# Dictionary with request data for each client (determined by ip).
+# By default, the dictionary contains a template that will be used to create other entries
+_requests_per_day = {
+    '127.0.0.1': {
+        'count': 1,
+        'date_last_requests': datetime.date.today(),
+    }
+}
+
+
+def _check_counter_date(client: dict) -> dict:
+    """ If the day of the last request does not coincide with the current, then the request counter is reset to 0. """
+    if client['date_last_requests'] != datetime.date.today():
+        client['date_last_requests'] = datetime.date.today()
+        client['count'] = 0
+    return client
+
+
+def get_client_ip(req) -> str:
+    """
+    This is used to get the user's IP from the request object.
+    """
+    x_forwarded_for = req.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = req.META.get('REMOTE_ADDR', "unknown")
+    return ip
+
+
+def check_requests_per_day(client_ip: str) -> int:
+    """ Here we get the number of user requests for the last day. """
+    if not isinstance(client_ip, str):
+        raise TypeError("The client_ip parameter must be str")
+    client = _requests_per_day.get(client_ip)
+    if not client:
+        return 0
+    client = _check_counter_date(client)
+    return client['count']
+
+
+def add_request_from_client(client_ip: str) -> int:
+    """ This function is used to increment the client's request counter (_requests_per_day variable). """
+    if not isinstance(client_ip, str):
+        raise TypeError("The client_ip parameter must be str")
+    client = _requests_per_day.get(client_ip)
+    if not client:
+        _requests_per_day[client_ip] = {
+            'count': 0,
+            'date_last_requests': datetime.date.today(),
+        }
+        client = _requests_per_day.get(client_ip)
+    client = _check_counter_date(client)
+    client['count'] += 1
+    return client['count']
+
+
+def clear_request_counter(client_ip: str) -> bool:
+    """ This function sets the number of requests from the passed client ip to 0 (_requests_per_day variable). """
+    if not isinstance(client_ip, str):
+        raise TypeError("The client_ip parameter must be str")
+    client = _requests_per_day.get(client_ip)
+    if not client:
+        return False
+    client['count'] = 0
+    return True
--- robin/views.py
+++ robin/views.py
@@ -5,28 +5,35 @@ from django.shortcuts import render, HttpResponse
 from django.contrib import messages
 from .forms import CaptchaForm
 from .models import CopySite
+from .services import *
 
 
 def index(request):
     site_url = request.POST.get('site-url', False)
+    captcha_form = CaptchaForm()
     if site_url:
+        client_ip = get_client_ip(request)
         form = CaptchaForm(request.POST or None)
         if form.is_valid():
+            if check_requests_per_day(client_ip) > 4:
+                messages.error(request, 'Вы не можете использовать сервис более 3\'х раз в сутки!')
+                return render(request, 'robin/index.html', {'captcha_form': captcha_form})
             try:
                 html_data = requests.get(site_url).text
                 hash = hashlib.sha3_512(html_data.encode('utf-8')).hexdigest()
                 copy_site = CopySite(hash_text=hash, url=site_url)
                 copy_site.save()
+
+                add_request_from_client(client_ip)
                 return render(request, 'robin/url_page.html', {'html_data': html_data, 'hash': copy_site.hash_text})
             except requests.exceptions.ConnectionError:
                 messages.error(request, 'Превышено время ожидания подключения к серверу!')
             except (requests.exceptions.InvalidURL, requests.exceptions.MissingSchema):
                 messages.error(request, 'Неверный URL ресурса!')
-            # except:
-            #     messages.error(request, 'Произошла ошибка, повторите попытку!')
+            except:
+                messages.error(request, 'Произошла ошибка, повторите попытку!')
 
         else:
             messages.error(request, 'Вы неверно ввели капчу!')
 
-    captcha_form = CaptchaForm()
     return render(request, 'robin/index.html', {'captcha_form': captcha_form})
